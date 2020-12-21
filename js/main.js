6560e63c07872de5eff0ad97a0a60242e01791f2
Aleksa Kojadinovic <aleksakojadinovic14@gmail.com>
2020-12-21 11:27:24 +0100

    Dodat alert

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var katalog=document.getElementById('katalog');
+var kontakt=document.getElementById('kontakt');
+ 
+katalog.onclick=function(){
+     alert("Aleksa Kojadinovic");
+};
+ 
+kontakt.onclick=function(){
+     alert("Kako ste?");
+};
